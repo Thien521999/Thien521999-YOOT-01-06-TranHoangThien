036aafaf9ba8fab45e7c3d9696169323c7cb0d17
hoangthientran <hoangthien.tran@m.citynow.vn>
2021-09-18 11:56:14 +0700

    first commit

--- 04/main.js
+++ 04/main.js
@@ -1,15 +1,16 @@
 const rotate90Clockwise = (arr) => {
   const n = arr.length;
-  for (i = n - 1; i >= 0; i--) {
-    for (j = 0; j < n; j++) document.write(arr[j][i] + " ");
+  for (let i = n - 1; i >= 0; i--) {
+    for (let j = 0; j < n; j++) document.write(arr[j][i] + " ");
     document.write("<br/>");
   }
 };
 
 var arr = [
-  [1, 2, 3, 4],
-  [5, 6, 7, 8],
-  [9, 10, 11, 12],
-  [13, 14, 15, 16],
+  [1, 825, 9, 6],
+  [85, 96, 1, 0],
+  [9, 125, 987, 5],
+  [8, 56, 32, 0],
 ];
+
 rotate90Clockwise(arr);
